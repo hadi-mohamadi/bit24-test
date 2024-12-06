4e69d628c8025ec285ff63271618cb35458eec52
hadi <work.hadimohammadi@gmail.com>
2024-12-07 00:27:20 +0330

    feature: added test for viewModel

--- features/stations/build.gradle.kts
+++ features/stations/build.gradle.kts
@@ -55,6 +55,9 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
+    testImplementation(libs.androidx.core.testing)
+    testImplementation(libs.kotlinx.coroutines.test)
+    testImplementation(libs.mockk)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
 
